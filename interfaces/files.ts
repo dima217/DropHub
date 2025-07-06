7cda148f9c374525fc820350c4f000a18bca4dbb
dima217 <dimapikulik217@mail.ru>
2025-07-06 17:45:22 +0300

    Initial commit

---
+++ interfaces/files.ts
@@ -0,0 +1,19 @@
+interface SharedFile {
+    id: string;
+    originalName: string;
+    storedName: string;
+    size: number;
+    mimeType: string;
+    uploadTime: Date;
+    downloadCount: number;
+    uploaderIp?: string;
+    expiresAt?: Date;   
+}
+
+interface UploadSession {
+    id: string;
+    files: string[];
+    createdAt: Date;
+    expiresAt?: Date;
+}
+  
