dc528d052cca9e9f1bedef8fd7310e9906c401e5
dima217 <dimapikulik217@mail.ru>
2025-09-07 17:55:14 +0300

    refactor: remove useless worker files

--- src/workers/file-cleanup/main.worker.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { FileCleanAppModule } from "./file.queue.module";
-
-async function bootstrap() {
-    const app = await NestFactory.createApplicationContext(FileCleanAppModule);
-    console.log('Worker running...');
-}
-bootstrap();
