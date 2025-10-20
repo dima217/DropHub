186d3c872e5cc52e166aca66fdce250127ffde29
dima217 <dimapikulik217@mail.ru>
2025-10-20 13:17:59 +0300

    chore(TokenController): creation of revoke token endpoint

--- src/modules/token/controllers/token.controller.ts
+++ src/modules/token/controllers/token.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { TokenService } from '../services/token.service';
 import { GenerateTokenDto } from '../dto/generate-token.dto';
+import { RevokeTokenDto } from '../dto/revoke-token.dto';
 
 @Controller('/token')
 export class TokenController {
   constructor(private readonly tokenService: TokenService) {}
-  @Post('generate')
+  @Post('/generate')
   async generateToken(@Body() generateTokenDto: GenerateTokenDto) {
     const token = await this.tokenService.generateToken(generateTokenDto);
     return { success: true, token: token };
+  }
+
+  @Post('/revoke')
+  async revokeToken(@Body() revokeTokenDto: RevokeTokenDto) {
+    await this.tokenService.revokeToken(revokeTokenDto.token);
   }
 }
