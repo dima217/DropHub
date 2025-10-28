a5225f902b1adb45e170f37726a25a1c674ec2bd
dima217 <dimapikulik217@mail.ru>
2025-10-28 11:00:16 +0300

    chore(Centrifugo): implementation of centrifugo module

--- src/modules/relationships/relationships.module.ts
+++ src/modules/relationships/relationships.module.ts
@@ -5,9 +5,10 @@ import { FriendRequest } from './entities/friend-request.entity';
 import { Friend } from './entities/friend.entity';
 import { RelationshipsService } from './services/relationships.service';
 import { UserModule } from '../user/user.module';
+import { CentrifugoModule } from '../notification/centrifugo.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FriendRequest, Friend]), UserModule],
+  imports: [TypeOrmModule.forFeature([FriendRequest, Friend]), UserModule, CentrifugoModule],
   controllers: [RelationshipsController],
   providers: [RelationshipsService],
   exports: [RelationshipsService],
